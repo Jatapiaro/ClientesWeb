b262b806d5e5ecb9904e275ab9a11a911de82717
Jacobo Tapia <jacob.et.cetera@gmail.com>
2018-11-22 16:49:46 -0600

    Añade cliente para wsdl de la práctica guiada

--- src/java/com/jacobo/controllers/WsdlTutorialController.java
+++ src/java/com/jacobo/controllers/WsdlTutorialController.java
@@ -6,16 +6,20 @@
 package com.jacobo.controllers;
 
 import https.footballpool_dataaccess.ArrayOftTeamCoachName;
+import https.footballpool_dataaccess.ArrayOftTeamPlayerName;
 import https.footballpool_dataaccess.Info;
 import https.footballpool_dataaccess.TTeamCoachName;
+import https.footballpool_dataaccess.TTeamPlayerName;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.xml.ws.WebServiceRef;
+import security.bypass.SSLUtilities;
 
 /**
  *
@@ -29,22 +33,37 @@ public class WsdlTutorialController implements Serializable {
     private Info service;
     
     private StringBuilder defaultPath;
-    private String test = "sdadsd";
+    private String country = "Mexico";
+    private List<String> countries;
+    private List<TTeamPlayerName> players;
     
-    public String getTest() {
-        return this.test;
-    }
     
     @PostConstruct
     public void init() {
-        List<TTeamCoachName> coaches = this.allTeamCoachNames().getTTeamCoachName();
-        for( TTeamCoachName c : coaches ) {
-            System.out.println(c.getSTeamName());
-        }
+        
+        //Disable all SSL security
+        SSLUtilities.trustAllHostnames();
+        SSLUtilities.trustAllHttpsCertificates();
+        
+        //Store context path for back button
         defaultPath = new StringBuilder();
         defaultPath.append(FacesContext.getCurrentInstance().getExternalContext().getApplicationContextPath());
+        
+        this.countries = new ArrayList<String>();
+        List<TTeamCoachName> coaches = this.allTeamCoachNames().getTTeamCoachName();
+        for( TTeamCoachName c : coaches ) {
+            getCountries().add(c.getSTeamName());
+        }
+        this.onCountryChange();
+        
     }
 
+    /*
+    * Return all the team coaches
+    * Used to get all the countries,
+    * we will just use the country
+    * @return ArrayOftTeamCoachName
+    */
     private ArrayOftTeamCoachName allTeamCoachNames() {
         // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
         // If the calling of port operations may lead to race condition some synchronization is required.
@@ -52,8 +71,66 @@ public class WsdlTutorialController implements Serializable {
         return port.allTeamCoachNames();
     }
     
+    private ArrayOftTeamPlayerName allPlayersWithRole(java.lang.String sTeamName, java.lang.String sRoleCode) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        https.footballpool_dataaccess.InfoSoapType port = service.getInfoSoap();
+        return port.allPlayersWithRole(sTeamName, sRoleCode);
+    }
+   
+    
+    
+    public void onCountryChange() { 
+        this.players = this.allPlayersWithRole(this.country, "defender").getTTeamPlayerName();
+    }
+    
+    /*
+    * Handles back button action
+    */
     public void goToIndex() throws IOException {
         FacesContext.getCurrentInstance().getExternalContext().redirect(this.defaultPath.toString());
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the countries
+     */
+    public List<String> getCountries() {
+        return countries;
+    }
+
+    /**
+     * @param countries the countries to set
+     */
+    public void setCountries(List<String> countries) {
+        this.countries = countries;
+    }
+
+    /**
+     * @return the players
+     */
+    public List<TTeamPlayerName> getPlayers() {
+        return players;
+    }
+
+    /**
+     * @param players the players to set
+     */
+    public void setPlayers(List<TTeamPlayerName> players) {
+        this.players = players;
     }
     
 }
